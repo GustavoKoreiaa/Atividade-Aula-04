02ee0d5e4f67aac30bd7a77e864a6e17966b197d
GustavoKoreiaa <106408467+GustavoKoreiaa@users.noreply.github.com>
2022-10-27 17:24:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var signo = prompt ("Digite seu signo");
+switch (signo){
+    case  'aries':
+        console.log('de 21 de março a 20 de abril');
+    break;
+    case 'touro':
+        console.log('de 21 abril a 20 maio');
+    break;
+    case 'gemeos':
+        console.log(' de 21 maio a 20 junho');
+    break;
+    case 'cancer':
+        console.log('de 21 junho a 22 julho');
+    break;
+    case 'leao':
+        console.log('de 23 julho a 22 agosto');
+    break;
+    case'virgem':
+    console.log(' de 23 agosto a 22 setembro');
+
+   default: 'Caso não tenha o signo'
+    console.log('Signo não registrado');
+    break;
+}
